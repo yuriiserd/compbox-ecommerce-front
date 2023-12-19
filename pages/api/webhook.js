47c7fc779ff8e80274fa4c2a964937cff4edf24d
yura <yura@socialfix.com>
2023-12-19 21:14:37 +0200

    attempt to fix webhook

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
   
   try {
     const buf = await buffer(req);
-    event = stripe.webhooks.constructEvent(buf.toString(), sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
   } catch (err) {
     console.log(err.message)
     res.status(400).send(`Webhook Error: ${err.message}`);
@@ -43,6 +43,6 @@ export default async function handler(req, res) {
 
 //account id => acct_1NvlidE4pCafpGtV
 
-export const config = {
-  api: {bodyParser: false}
-}
+// export const config = {
+//   api: {bodyParser: false}
+// }
