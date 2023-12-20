25cbaaba68e7e3bb7d783e1a879acb2b609a8d3a
yura <yura@socialfix.com>
2023-12-20 13:38:06 +0200

    add google analytics

--- components/LayoutNoPreloader.js
+++ components/LayoutNoPreloader.js
@@ -1,17 +1,10 @@
 import Container from "./Container";
 import Footer from "./Footer";
-import { useEffect } from "react";
 
 import Header from "./Header";
-import { initGA, logPageView } from "@/services/analytics";
 
 
 export default function LayoutNoPreloader({children}) {
-
-  useEffect(() => {
-    initGA();
-    logPageView();
-  }, [])
 
   return (
     <>
